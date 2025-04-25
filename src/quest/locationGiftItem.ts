2ec89e089b70b4344d1f1ebd8707c01072b813ec
totl <totl@soton.ac.uk>
2025-04-25 09:23:18 +0100

    better-gifts

--- src/quest/locationGiftItem.ts
+++ src/quest/locationGiftItem.ts
@@ -7,14 +7,14 @@ import {generateRandomItem} from "@/quest/generateRandomItem.ts";
  * @returns An item or undefined if no gift should be generated
  */
 export function locationGiftItem(difficulty: GameLocationDifficulty): Item | undefined {
-    // Easy locations always have a level 2 gift
+    // Easy locations always have a level 4 gift
     if (difficulty === 'easy' || difficulty === 'start') {
-        return generateRandomItem(2);
+        return generateRandomItem(4);
     }
 
-    // Medium locations have a 50% chance of a level 3 gift
+    // Medium locations have a 50% chance of a level 5 gift
     if (difficulty === 'medium') {
-        return Math.random() < 0.5 ? generateRandomItem(3) : undefined;
+        return Math.random() < 0.5 ? generateRandomItem(5) : undefined;
     }
 
     // Hard and end locations have no gift
